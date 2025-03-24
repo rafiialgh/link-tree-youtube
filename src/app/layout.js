04f897d8fd255facd192af6b24a46d31bea4705d
rafiialgh <rafii.alghafari@gmail.com>
2025-03-24 15:38:41 +0700

    edit meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,29 @@
 import './globals.css'
-import {Inter} from 'next/font/google'
+import { Inter } from 'next/font/google'
 import Head from "next/head";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Link Youtube-nya Rafii',
   description: 'Kalo mau liat video di Youtube gueh 😎',
 }
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-    <head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <link rel="manifest" href="/site.webmanifest"/>
-      <title></title>
-    </head>
-    <body className={`${inter.className}`}>{children}</body>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body className={`${inter.className} bg-black`}>{children}</body>
     </html>
   )
 }
